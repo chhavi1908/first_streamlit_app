fdad8fc01356441eaba13f5d9f52fe2d63e3834a
chhavi1908 <138116527+chhavi1908@users.noreply.github.com>
2023-06-29 23:26:39 +0530

    Update stream_lit_app.py

--- stream_lit_app.py
+++ stream_lit_app.py
@@ -1,8 +1,10 @@
 import streamlit
 streamlit.title('my parents new healthy diner')
-streamlit.header('breakfast menu')
+streamlit.header('breakfast Favorites')
 
 streamlit.text('🥣omega 3 & blueberry oatmeal')
 streamlit.text('🥗 kale,spinach and rocket smoothie')
 streamlit.text('🐔 hard boiled free-ragne eggs')
 streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 build your own fruit smoothie 🥝🍇')
